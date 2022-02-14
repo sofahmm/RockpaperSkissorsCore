55eb5534d033afba771889b50a6046870c7a4fdb
sofahmm <74815523+sofahmm@users.noreply.github.com>
2022-02-14 08:51:30 +0300

    Add project files.

---
+++ RockpaperSkissorsCore/Figure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockpaperSkissorsCore
+{
+    class Figure
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
